209bee5dc54914c7e087b339f9fb9a9a684cf0e9
tonywang <tonywang@yuda.tyc.edu.tw>
2020-06-02 15:36:01 +0800

    ㄩ

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 # 導入 browser 的 document 及 html 物件
-from browser import document, alert, console, load
-load("https://cdnjs.cloudflare.com/ajax/libs/tone/14.7.3/Tone.js")
-Tone = window.Tone
+# from browser import document, alert, console, load
+
+# tone = window.Tone
 
 
 
 
-def action1(ev):
-    alert("Hello !")
-    print("Hello2 !")
-    synth = new Tone.Synth().toMaster()
-    //play music
-    synth.triggerAttackRelease('C4', '8n')
+# def action1(ev):
+#     alert("Hello !")
+#     print("Hello2 !")
+#     synth = new tone.Synth().toMaster()
+#     //play music
+#     synth.triggerAttackRelease('C4', '8n')
 
-document["box1"].bind("click", action1)
+# document["box1"].bind("click", action1)
+
+
+from browser import document, window
+
+THREE = window.THREE
+
+camera = THREE.PerspectiveCamera.new(75, 1, 1, 10000)
+camera.position.z = 1000
+scene = THREE.Scene.new()
+geometry = THREE.CubeGeometry.new(200, 200, 200)
+material = THREE.MeshBasicMaterial.new({"color": "#ff0000", "wireframe": True})
+mesh = THREE.Mesh.new(geometry, material)
+scene.add(mesh)
+
+renderer = THREE.WebGLRenderer.new()
+renderer.setSize(444, 444)
+
+document <= renderer.domElement
+renderer.render(scene, camera)
+
+def animate(i):
+    # note: three.js includes requestAnimationFrame shim
+    window.requestAnimationFrame(animate)
+
+    mesh.rotation.x += 0.01
+    mesh.rotation.y += 0.02
+
+    renderer.render(scene, camera)   
+
+animate(0)
