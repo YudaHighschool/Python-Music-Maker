4eb62a7dfd19fe95673f75d59f7908279731728f
tonywang <tonywang@yuda.tyc.edu.tw>
2020-06-02 15:39:32 +0800

    m

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-# 導入 browser 的 document 及 html 物件
-# from browser import document, alert, console, load
-
-# tone = window.Tone
-
-
-
-
-# def action1(ev):
-#     alert("Hello !")
-#     print("Hello2 !")
-#     synth = new tone.Synth().toMaster()
-#     //play music
-#     synth.triggerAttackRelease('C4', '8n')
-
-# document["box1"].bind("click", action1)
- s
-
 from browser import document, window
 
 THREE = window.THREE
