4de76c9e40ea2740d1db81156e6136f7a8422193
tonywang <tonywang@yuda.tyc.edu.tw>
2020-06-02 15:10:04 +0800

    m

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # 導入 browser 的 document 及 html 物件
-from browser import document, alert
+from browser import document, alert, console
 
 def action1(ev):
     alert("Hello !")
+    print("Hello2 !")
 
 document["box1"].bind("click", action1)
 
